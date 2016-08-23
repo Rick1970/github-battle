f7019c1822b4433afc1aaee992b67419cd1de3e1
Rick1970 <rthornbrug@gmail.com>
2016-08-23 13:15:42 -0700

    work on to do list

--- angular-practice/app/app.component.ts
+++ angular-practice/app/app.component.ts
@@ -1,48 +1,31 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'task-list',
-  inputs: ['taskList'],
-  template:`
-  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
-  {{currentTask.description}}
-  </h3>
-  `
-})
-export class TaskListComponent {
-  public taskList: Task[];
-  taskClicked(clickedTask: Task): void {
-  console.log(clickedTask);
-  }
-}
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   directives: [TaskListComponent],
   template: `
-  <div class="container">
-  <h1>To-Do List</h1>
-  <task-list [taskList]="tasks"></task-list>
-  </div>
+    <div class="container">
+      <h1>To-Do List</h1>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    <div>
   `
 })
 export class AppComponent {
-  public task: Task[];
+  public tasks: Task[];
   constructor(){
-  this.task = [                      new Task('Create To-Do List app.', 0),
-  new Task('Learn Kung Fu', 1),
-  new Task('Rewatch LOR movies', 2),
-  new Task('Do the laundry.', 3)
-  ];
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
   }
   taskWasSelected(clickedTask: Task): void {
-  console.log(clickedTask);
+    console.log('parent', clickedTask);
   }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number){
-
-}
 }
